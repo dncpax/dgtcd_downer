8048f5b32d09bc6f37573eb4eb59ff92510b2a52
ulmi <ulmi.giga@gmail.com>
2025-07-09 16:21:19 +0100

    1st version of the plugin for qgis, ported from the script

---
+++ plugin_qgis/dgt_cdd_downloader_plugin.py
@@ -0,0 +1,54 @@
+from qgis.core import QgsApplication, QgsMessageLog, Qgis
+
+# Import the provider
+from .dgt_cdd_downloader_provider import DgtCddDownloaderProvider
+
+# Check for dependencies
+try:
+    import requests
+    REQUESTS_AVAILABLE = True
+except ImportError:
+    REQUESTS_AVAILABLE = False
+
+class DgtCddDownloaderPlugin:
+    """QGIS Plugin Implementation."""
+
+    def __init__(self, iface):
+        """
+        Constructor.
+        :param iface: An interface instance that will be passed to this class
+            which provides access to the QGIS application settings and main
+            interface objects.
+        """
+        self.iface = iface
+        self.provider = None
+
+    def initGui(self):
+        """
+        Called when the plugin is loaded in QGIS.
+        """
+        if not REQUESTS_AVAILABLE:
+            self.iface.messageBar().pushMessage(
+                "Warning",
+                "DGT CDD Downloader plugin requires the 'requests' library. Please install it.",
+                level=Qgis.Critical,
+                duration=15
+            )
+            QgsMessageLog.logMessage(
+                "DGT CDD Downloader: Could not import 'requests'. The plugin will be disabled. "
+                "Please install it in your QGIS Python environment (e.g., 'py3-pip install requests' in OSGeo4W Shell).",
+                'DGT CDD Downloader',
+                level=Qgis.Critical
+            )
+            return
+
+        self.provider = DgtCddDownloaderProvider()
+        QgsApplication.processingRegistry().addProvider(self.provider)
+
+    def unload(self):
+        """
+        Called when the plugin is unloaded.
+        """
+        if self.provider:
+            QgsApplication.processingRegistry().removeProvider(self.provider)
+            self.provider = None
---
+++ plugin_qgis/dgt_cdd_downloader_provider.py
@@ -0,0 +1,53 @@
+from qgis.core import QgsProcessingProvider
+from qgis.PyQt.QtGui import QIcon
+import os
+
+# Import the algorithm from the processing_algorithm.py file
+from .processing_algorithm import DgtCddDownloaderAlgorithm
+
+class DgtCddDownloaderProvider(QgsProcessingProvider):
+    """
+    This class registers the DGT CDD Downloader algorithm with the Processing framework.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.algorithms = []
+
+    def loadAlgorithms(self, *args, **kwargs):
+        """
+        Loads all algorithms belonging to this provider.
+        """
+        self.algorithms = [DgtCddDownloaderAlgorithm()]
+        for alg in self.algorithms:
+            self.addAlgorithm(alg)
+
+    def id(self, *args, **kwargs):
+        """
+        Returns the unique provider id.
+        """
+        return 'dgt_cdd_downloader_provider'
+
+    def name(self, *args, **kwargs):
+        """
+        Returns the provider name, used for the group in the Processing toolbox.
+        """
+        return self.tr("DGT CDD Portal")
+
+    def longName(self, *args, **kwargs):
+        """
+        Returns a longer version of the provider name.
+        """
+        return self.name()
+
+    def icon(self):
+        """
+        Returns the provider's icon.
+        """
+        icon_path = os.path.join(
+            os.path.dirname(__file__),
+            'icon.png'
+        )
+        if os.path.exists(icon_path):
+            return QIcon(icon_path)
+        return QIcon()
